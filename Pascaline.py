651e7ff2d2359e1e18f7c628d3edfc3aedca4ed8
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-01-13 08:45:29 +0100

    creation des imput avec vérication que ça soit bon et range selon priorité

--- Pascaline.py
+++ Pascaline.py
@@ -1,5 +1,8 @@
 import re
 
+val_symbole={'+':3, '-':3, '*':2, '/':2, '^':2, '(': '1', ')':'1'}
+
+
 symboles = ['+', '-', '*', '/', '(', ')', '^', '.'] 
 def input_calcul():
    while True:
@@ -17,10 +20,24 @@ def input_calcul():
             if i in symboles:
                     globals()[f'symbole_{n}']=i
                     n+=1                    
-        calculeN=re.split(r'(\+|\-|\*|\/|\(|\)|\^|\.)', calcule)
-        return calculeN
-        
-        
+        calculeN=re.split(r'(\+|\-|\*|\/|\(|\)|\^)', calcule)
+        n1=n2=r1=None
+        op=[]
+        n=[]
+        for i in calculeN:
+            if i in symboles:
+                op.append(i)
+            else:
+                n.append(float(i))
+        for i in range(len(op)):
+            for j in range(len(op)):
+                if val_symbole[op[i]]<val_symbole[op[j]]:
+                    op[i],op[j]=op[j],op[i]
+                    n[j],n[i+1]=n[i+1],n[j]
+        return (n,op)            
                     
+calcul=input_calcul()
+print(calcul)      
+                   
         
           
