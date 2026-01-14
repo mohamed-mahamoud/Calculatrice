63baa075349222c8faff1a9df032cafb506299ad
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-01-14 09:34:43 +0100

    rajout de calcule et des erreur

--- calculatrice_test.py
+++ calculatrice_test.py
@@ -1,7 +1,8 @@
 import re
 
+
 val_symbole = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
-symboles = ['+', '-', '*', '/', '(', ')', '^', '.']
+symboles = ['+', '-', '*', '/', '(', ')', '^', '.', '%', '//', 'V', '!']
 
 def input_calcul(calcul=None, pos=0, niveau='input'):
     # Niveau input : demander le calcul
@@ -10,11 +11,24 @@ def input_calcul(calcul=None, pos=0, niveau='input'):
             invalide = False
             calcul = input("Entrez le calcul à effectuer : ")
             
-            for i in calcul:
-                if i not in '0123456789+-*/()^.% ':
-                    print("Caractère non autorisé détecté.")
-                    invalide = True
-                    break
+            i = 0
+            while i < len(calcul):
+                c = calcul[i]
+                if c.isdigit() or c in '+-()*^.% V!':
+                    i += 1
+                    continue
+                if c == '/':
+                    if i + 1 < len(calcul) and calcul[i + 1] == '/':
+                        i += 2
+                        continue
+                    i += 1  # single '/'
+                    continue
+                if c.isspace():
+                    i += 1
+                    continue
+                print("Caractère non autorisé détecté.")
+                invalide = True
+                break
             if invalide:
                 continue
             
@@ -24,26 +38,11 @@ def input_calcul(calcul=None, pos=0, niveau='input'):
             
             try:
                 calcul = calcul.replace(' ', '')
-                arbre = input_calcul(calcul, 0, 'add')[0]
-                n, op = input_calcul(arbre, 0, 'extraire')
-                return (n, op)
+                arbre, _ = input_calcul(calcul, 0, 'add')
+                return arbre
             except Exception as e:
                 print(f"Erreur dans l'expression : {e}")
                 continue
-    
-    # Niveau extraire : extraire n et op de l'arbre
-    elif niveau == 'extraire':
-        n = []
-        op = []
-        def ex(a):
-            if isinstance(a, tuple):
-                ex(a[1])
-                ex(a[2])
-                op.append(a[0])
-            else:
-                n.append(a)
-        ex(calcul)
-        return n, op
     
     # Niveau add : addition/soustraction
     elif niveau == 'add':
@@ -58,17 +57,31 @@ def input_calcul(calcul=None, pos=0, niveau='input'):
     # Niveau mult : multiplication/division
     elif niveau == 'mult':
         g, pos = input_calcul(calcul, pos, 'puis')
-        while pos < len(calcul) and calcul[pos] in '*/':
-            o = calcul[pos]
-            pos += 1
+        while pos < len(calcul):
+            if calcul.startswith('//', pos):
+                o = '//'
+                pos += 2
+            elif calcul[pos] in '*/':
+                o = calcul[pos]
+                pos += 1
+            else:
+                break
             d, pos = input_calcul(calcul, pos, 'puis')
             g = (o, g, d)
         return g, pos
     
+    # Niveau postfix : factorielle (postfixe unaire)
+    elif niveau == 'postfix':
+        g, pos = input_calcul(calcul, pos, 'base')
+        while pos < len(calcul) and calcul[pos] == '!':
+            g = ('!', g)
+            pos += 1
+        return g, pos
+    
     # Niveau puis : puissance
     elif niveau == 'puis':
-        g, pos = input_calcul(calcul, pos, 'base')
-        if pos < len(calcul) and calcul[pos] == '^':
+        g, pos = input_calcul(calcul, pos, 'postfix')
+        if pos < len(calcul) and calcul[pos] in '^V':
             o = calcul[pos]
             pos += 1
             d, pos = input_calcul(calcul, pos, 'puis')
@@ -109,13 +122,25 @@ def multiple (a,b):
     return a*b
 
 def div (a,b):
-    return a/b
+    if b == 0:
+        print("Erreur : Division par zéro")
+        calcul()
+    else:
+        return a/b
 
 def modulo (a,b):
-    return a%b
+    if b == 0:
+        print("Erreur : Division par zéro")
+        calcul()
+    else:
+        return a%b
 
 def div_euclidienne(a,b):
-    return a//b
+    if b == 0:
+        print("Erreur : Division par zéro")
+        calcul()
+    else:
+        return a//b
 
 def puissance(a,b):
     return a**b
@@ -130,52 +155,69 @@ def racine (a,b):
     return a**(1/b)
 
 def factoriel(a):
-    resultat=0
-    for i in range(a):
-        if i==0:
-            resultat+=a
-        else:
-            resultat*=a-i
+    a = int(a)
+    if a < 0:
+        raise ValueError("Factorielle d'un nombre negatif")
+    if a == 0 or a == 1:
+        return 1
+    resultat = 1
+    for i in range(2, a + 1):
+        resultat *= i
     return resultat
 
 #======== Reconnaisance de calcul =========#
 
 
-def calcul() :
-    n, op = input_calcul()
-    index_num_actu = 0
-    list_index_fini = []
-    print(n)
-    for operateur in op :
-        for i in range(len(op)) :
-            if op[i] == operateur :
-                list_index_fini.append(i)
-                list_index_fini.append(i+1)
-                match op[i] :
-                    case '*' :
-                        calc = multiple(n[i], n[i+1])
-                        for index in list_index_fini :
-                            n[index] = calc
-                        print(n)
-                    case '/' :
-                        calc = div(n[i], n[i+1])
-                        for index in list_index_fini :
-                            n[index] = calc
-                        print(n)
-                    case '+' :
-                        calc = add(n[i], n[i+1])
-                        for index in list_index_fini :
-                            n[index] = calc
-                        print(n)
-                    case '-' :
-                        calc = sous(n[i], n[i+1])
-                        for index in list_index_fini :
-                            n[index] = calc
-                        print(n)
-
-    return n[-1]
-
-
+def calcul():
+    arbre = input_calcul()
+    
+    def evaluer(a):
+        if isinstance(a, tuple):
+            if len(a) == 2:
+                # Operateur unaire (postfixe)
+                operateur, operande = a
+                op = evaluer(operande)
+                
+                match operateur:
+                    case '!':
+                        calc = factoriel(op)
+                    case _:
+                        calc = op
+                return calc
+            else:
+                # Operateur binaire
+                operateur, gauche, droit = a
+                g = evaluer(gauche)
+                d = evaluer(droit)
+                
+                match operateur:
+                    case '*':
+                        calc = (multiple(g, d))
+                    case '/':
+                        calc = (div(g, d))
+                    case '+':
+                        calc = (add(g, d))
+                    case '-':
+                        calc = (sous(g, d))
+                    case '%': 
+                        calc = (modulo(g, d))
+                    case '//': 
+                        calc = (div_euclidienne(g, d))
+                    case '^':
+                        calc = (puissance(g, d))
+                    case 'V':
+                        calc = (racine(g, d))
+                
+                return calc
+        else:
+            return a
+    
+    resultat = evaluer(arbre)
+    if isinstance(resultat, float) and resultat.is_integer():
+        return int(resultat)
+    return resultat
 #======== MAIN ==========#
-print(calcul())
+
+resultat = calcul()
+print(resultat)
 #in_list_calc(chaine)
